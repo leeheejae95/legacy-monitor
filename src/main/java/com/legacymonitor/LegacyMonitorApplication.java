a122aa60e5fde90c6ff2aff83a94c3751dd25ae5
leeheejae95 <leeheejae55@naver.com>
2026-09-10 16:07:10 +0900

    init: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/legacymonitor/LegacyMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.legacymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legacymonitor/LegacyMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
